36ee14ba68a976427dccee726f178127cd0a3ed3
Keith Yao <78155676+AgentKettlepot@users.noreply.github.com>
2023-01-24 05:48:47 -0500

    Added add to db and routing

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import json
 from flask import request, Flask, render_template, jsonify
 from CollegexTinder.CollegeTinder import CollegeTinder
+import csv
 CT = CollegeTinder(0.01)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to My Website!"
+    return render_template('home.html')
 
 
 @app.route('/tester')
@@ -22,6 +23,19 @@ def test_func():
     print(input_vector) # This is the output that was stored in the JSON within the browser
     rank = CT.run(input_vector)
     print(rank)
+    return json.dumps({'status':'OK','rank':rank});
+
+
+@app.route('/write_to_db', methods=['POST'])
+def write():
+    output = request.get_json()
+    result = json.loads(output)
+    user_inputs = list(result.values())
+    print(user_inputs)
+    with open ("CollegexTinder\\csv\\USER_DATA.csv", "r+", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(user_inputs)
+    
     return json.dumps({'status':'OK','rank':'testtesttest'});
 
 
--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
-const button = document.getElementById("Submit");
+const sub_button = document.getElementById("Submit");
 const data = document.getElementById("info");
 
-    button.onclick = function(){
+    sub_button.onclick = function(){
         var major_code = document.getElementById("m_code").value;
         var area = document.getElementById("area").value;
         var l_cost = document.getElementById("l_cost").value;                
@@ -30,7 +30,35 @@ const data = document.getElementById("info");
             error: function(error){
                 console.log(error)
             }
-        });
+        }); 
+}
 
-      
+const add_button = document.getElementById('Add_Button');
+add_button.onclick = function(){
+    var major_code = document.getElementById("m_code").value;
+    var area = document.getElementById("area").value;
+    var l_cost = document.getElementById("l_cost").value;                
+    var h_cost = document.getElementById("h_cost").value;
+    var miles = document.getElementById("miles_from_home").value;                
+    var SAT = document.getElementById("sat").value;
+    var GPA = document.getElementById("gpa").value;                
+    var l_accept = document.getElementById("l_acceptance").value;
+    var h_accept = document.getElementById("h_acceptance").value;    
+    var zip = document.getElementById("zip_code").value;
+    const dict_values = {major_code, area, l_cost, h_cost, miles, SAT, GPA, l_accept, h_accept, zip}
+    const s = JSON.stringify(dict_values);
+    console.log(s); 
+
+    $.ajax({
+        url:"/write_to_db",
+        type:"POST",
+        contentType: "application/json",
+        data: JSON.stringify(s),
+        success: function(result) {
+            console.log(result);
+        },
+        error: function(error){
+            console.log(error)
+        }
+    }); 
 }
